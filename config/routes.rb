3aa3eaa43909f1c915e2690531dd9889b164c839
Juliette Bayard <juliette.bayard8@gmail.com>
2018-11-20 14:56:49 +0100

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all tasks
+  get "/tasks", to: "tasks#index"
+  # Create one restaurant
+  get "/tasks/new", to: "tasks#new", as: "new"
+  post "/tasks", to: "tasks#create"
+  # Read one task
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  # Edit one restaurant
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "/tasks/:id", to: "tasks#update"
+  # Delete one restaurant
+  delete "/tasks/:id", to: "tasks#destroy", as: "destroy"
 end
